bab689ce18199e371a5ac3343594a0dffe1796d9
damianricobelli <dricobelli@gmail.com>
2026-02-10 16:58:21 -0300

    chore: remove unused assets and update root route metadata
    - Deleted obsolete image files (logo192.png, logo512.png, tanstack-circle-logo.png, tanstack-word-logo-white.svg) and the manifest.json to clean up the public directory.
    - Updated the root route metadata in __root.tsx to reflect the new application title and description, enhancing SEO and user clarity.

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -33,7 +33,14 @@ export const Route = createRootRouteWithContext<MyRouterContext>()({
         content: "width=device-width, initial-scale=1",
       },
       {
-        title: "TanStack Start Starter",
+        title: "Spenty",
+        description: "Control your expenses with Spenty",
+        keywords: "spenty, expenses, control, expenses control",
+        author: "Spenty",
+        publisher: "Spenty",
+        generator: "Spenty",
+        robots: "index, follow",
+        google: "notranslate",
       },
     ],
     links: [
